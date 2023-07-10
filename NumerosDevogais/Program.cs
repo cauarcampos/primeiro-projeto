17a1182d173504f4a2cab450d59518eb2f5580e4
Cauã <cauarcampos21@gmail.com>
2023-07-10 04:43:03 -0300

    subindo projeto numero de vogais

---
+++ NumerosDevogais/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite uma palavra: ");
+        string nome = Console.ReadLine();
+
+        int contador = 0;
+
+        foreach (var letra in nome)
+        {
+            if (letra == 'a' || letra == 'A' ||
+                letra == 'e' || letra == 'E' ||
+                letra == 'i' || letra == 'I' ||
+                letra == 'o' || letra == 'O' ||
+                letra == 'u' || letra == 'U')
+            {
+                contador++;
+            }
+        }
+
+        Console.WriteLine("Número de vogais é " + contador);
+       
+    }
+}
