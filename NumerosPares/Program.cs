89d119816abf4c343d23af20460ee9fbcdde67c5
Felipe Rocha <felipemaciel11111@gmail.com>
2023-07-23 03:01:22 -0300

    s

--- NumerosPares/Program.cs
+++ NumerosPares/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=== Esse programa imprime todos os numeros pares de 0 a 100 ===");
